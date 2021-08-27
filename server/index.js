11de48734b1aa1ffe83129a67dc9a87f479cb4e9
SangminSuk <81797919+SangminSuk@users.noreply.github.com>
2021-08-27 11:18:06 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
     cors({
-        origin: '*',
+        origin: true,
         credentials: true,
         methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     }),
