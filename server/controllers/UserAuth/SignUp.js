ee7251719adb8b219a96a02a05cd8744e340c623
SangminSuk <tjrtkdals96@gmail.com>
2021-08-27 13:39:07 +0900

    server test

--- server/controllers/UserAuth/SignUp.js
+++ server/controllers/UserAuth/SignUp.js
@@ -3,15 +3,16 @@ const { user } = require('../../models/user');
 module.exports = (req, res) => {
     const { nickName, email, password, mobile, picture, provider } = req.body;
 
-    user.findOne({ where: { email: email } }).then((emailConflict) => {
-        if (emailConflict) {
-            res.status(409).json({ message: 'email exist' })
-        }
-        user.create({ nickName, email, password, mobile, picture, provider })
-        .then((success) => {
-            res.status(201).json({ message: 'ok' });
+    user.findOne({ where: { email: email } })
+        .then((emailConflict) => {
+            if (emailConflict) {
+                res.status(409).json({ message: 'email exist' });
+            }
+            user.create({ nickName, email, password, mobile, picture, provider }).then((success) => {
+                res.status(201).json({ message: 'ok' });
+            });
         })
-    }).catch (err) {
-        console.log(err);
-    }
+        .catch((err) => {
+            console.log(err);
+        });
 };
--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
     cors({
-        origin: '*',
+        origin: true,
         credentials: true,
         methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     }),
@@ -49,6 +49,8 @@ app.all('/content', controllers.content);
 
 const PORT = 80;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log('열려라 서버!');
 });
+
+module.exports = server;
