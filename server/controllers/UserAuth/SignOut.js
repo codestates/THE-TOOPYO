17d1e71640d2070e58b59bdcca11c14caee6d063
SangminSuk <tjrtkdals96@gmail.com>
2021-08-27 09:29:30 +0900

    [server]signout

--- server/controllers/UserAuth/SignOut.js
+++ server/controllers/UserAuth/SignOut.js
@@ -1,1 +1,8 @@
-module.exports = (req, res) => {};
+module.exports = (req, res) => {
+    if (!req.session.email) {
+        res.status(400).json({ message: 'You`re currently not loged in' });
+    } else {
+        req.session.destroy();
+        res.json({ message: 'ok' });
+    }
+};
