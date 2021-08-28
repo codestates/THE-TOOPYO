fd0dff400e61926e5fa9d1704db6a55c63856c80
baemki <alsrlek123@gmail.com>
2021-08-28 22:12:25 +0900

    [client]css fix

--- client/src/components/modals/LoginModal.js
+++ client/src/components/modals/LoginModal.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import './LoginModal.css';
+import './Modal.css';
 
 function Login({ isOpen, close, loginHandler }) {
     const [loginInfo, setLoginInfo] = useState({
